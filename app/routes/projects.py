cc8a13c54e508a362a423b44e780b94238fe3b2d
harisandre81-max <harisandre81@gmail.com>
2026-05-25 22:21:24 -0600

    falta acomodar la visualizacion de los reportes

--- app/routes/projects.py
+++ app/routes/projects.py
@@ -4,9 +4,15 @@ from app.database.deps import get_db
 from app.schemas.project import ProjectCreate
 from app.services.project_service import create_project_service, get_projects_service
 from app.core.auth import get_current_user
-
+from app.schemas.project import ProjectResponse
 router = APIRouter(prefix="/projects", tags=["projects"])
 
+@router.get("/", response_model=list[ProjectResponse])
+def get_projects(
+    db: Session = Depends(get_db),
+    user = Depends(get_current_user)
+):
+    return get_projects_service(db, user)
 
 @router.post("/")
 def create_project(
@@ -15,11 +21,3 @@ def create_project(
     user = Depends(get_current_user)
 ):
     return create_project_service(db, data, user)
-
-
-@router.get("/")
-def get_projects(
-    db: Session = Depends(get_db),
-    user = Depends(get_current_user)
-):
-    return get_projects_service(db, user)
